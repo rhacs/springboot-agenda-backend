ecc44b9aab13783dcc8699d59bdf25d5d6be3c4c
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-05-16 16:36:29 -0400

    :tada: Initial Commit

---
+++ src/main/java/cl/rhacs/springboot/agenda/AgendaApplication.java
@@ -0,0 +1,13 @@
+package cl.rhacs.springboot.agenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgendaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cl/rhacs/springboot/agenda/AgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.rhacs.springboot.agenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
