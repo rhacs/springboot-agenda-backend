4036ed7cb9499092be94d44d415426ea2555ef68
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-05-16 21:06:44 -0400

    :bento: Adding initial demo data

---
+++ src/main/resources/data-mysql.sql
@@ -0,0 +1,130 @@
+--
+-- NOTE: All the data here was generated using the mockaroo.com web service
+--
+
+--
+-- Table: tags
+--
+
+insert into tags (name, created_at, updated_at) values ('Family', now(), now());
+insert into tags (name, created_at, updated_at) values ('Friend', now(), now());
+insert into tags (name, created_at, updated_at) values ('Coworker', now(), now());
+insert into tags (name, created_at, updated_at) values ('Boss', now(), now());
+insert into tags (name, created_at, updated_at) values ('Friend of a Friend', now(), now());
+insert into tags (name, created_at, updated_at) values ('Client', now(), now());
+insert into tags (name, created_at, updated_at) values ('Provider', now(), now());
+insert into tags (name, created_at, updated_at) values ('Touch and Go', now(), now());
+insert into tags (name, created_at, updated_at) values ('Snack', now(), now());
+insert into tags (name, created_at, updated_at) values ('Other', now(), now());
+
+--
+-- Table: contacts
+--
+
+insert into contacts (first_name, last_name, email, created_at, updated_at) values ('Odette', 'Joanaud', 'ojoanaud0@exblog.jp', now(), now());
+insert into contacts (first_name, last_name, email, created_at, updated_at) values ('Sarette', 'Cromer', 'scromer1@bloglines.com', now(), now());
+insert into contacts (first_name, last_name, email, created_at, updated_at) values ('Charlotta', 'Mathivon', 'cmathivon2@marketwatch.com', now(), now());
+insert into contacts (first_name, last_name, email, created_at, updated_at) values ('Roldan', 'Tomkins', 'rtomkins3@seattletimes.com', now(), now());
+insert into contacts (first_name, last_name, email, created_at, updated_at) values ('Mehetabel', 'Soden', 'msoden4@acquirethisname.com', now(), now());
+insert into contacts (first_name, last_name, email, created_at, updated_at) values ('Joell', 'Gatrill', 'jgatrill5@yellowbook.com', now(), now());
+insert into contacts (first_name, last_name, email, created_at, updated_at) values ('Elaina', 'O'' Connell', 'eoconnell6@cbsnews.com', now(), now());
+insert into contacts (first_name, last_name, email, created_at, updated_at) values ('Eleanore', 'Gambell', 'egambell7@cmu.edu', now(), now());
+insert into contacts (first_name, last_name, email, created_at, updated_at) values ('Dell', 'Hartgill', 'dhartgill8@facebook.com', now(), now());
+insert into contacts (first_name, last_name, email, created_at, updated_at) values ('Tiffani', 'Haldene', 'thaldene9@google.com', now(), now());
+insert into contacts (first_name, last_name, email, created_at, updated_at) values ('Nicko', 'Rustadge', 'nrustadgea@ask.com', now(), now());
+insert into contacts (first_name, last_name, email, created_at, updated_at) values ('Wendye', 'Stannislawski', 'wstannislawskib@google.co.jp', now(), now());
+insert into contacts (first_name, last_name, email, created_at, updated_at) values ('Evelyn', 'Bakes', 'ebakesc@tamu.edu', now(), now());
+insert into contacts (first_name, last_name, email, created_at, updated_at) values ('Arlinda', 'McKeaveney', 'amckeaveneyd@yellowbook.com', now(), now());
+insert into contacts (first_name, last_name, email, created_at, updated_at) values ('Lucais', 'Loakes', 'lloakese@technorati.com', now(), now());
+insert into contacts (first_name, last_name, email, created_at, updated_at) values ('Skyler', 'Roskam', 'sroskamf@biblegateway.com', now(), now());
+insert into contacts (first_name, last_name, email, created_at, updated_at) values ('Gilbert', 'Burdge', 'gburdgeg@whitehouse.gov', now(), now());
+insert into contacts (first_name, last_name, email, created_at, updated_at) values ('Rikki', 'Offa', 'roffah@marketwatch.com', now(), now());
+insert into contacts (first_name, last_name, email, created_at, updated_at) values ('Darby', 'Govinlock', 'dgovinlocki@ocn.ne.jp', now(), now());
+insert into contacts (first_name, last_name, email, created_at, updated_at) values ('Donielle', 'Larver', 'dlarverj@dailymail.co.uk', now(), now());
+insert into contacts (first_name, last_name, email, created_at, updated_at) values ('Abner', 'Malletratt', 'amalletrattk@wsj.com', now(), now());
+insert into contacts (first_name, last_name, email, created_at, updated_at) values ('Parry', 'Chretien', 'pchretienl@geocities.com', now(), now());
+insert into contacts (first_name, last_name, email, created_at, updated_at) values ('Mignonne', 'Speddin', 'mspeddinm@people.com.cn', now(), now());
+insert into contacts (first_name, last_name, email, created_at, updated_at) values ('Feliks', 'Rushworth', 'frushworthn@hugedomains.com', now(), now());
+insert into contacts (first_name, last_name, email, created_at, updated_at) values ('Norbie', 'Syms', 'nsymso@google.ru', now(), now());
+
+--
+-- Table: contact_notes
+--
+
+insert into contact_notes (content, contact_id, updated_at, created_at) values ('Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.', 7, now(), now());
+insert into contact_notes (content, contact_id, updated_at, created_at) values ('Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis. Fusce posuere felis sed lacus.', 13, now(), now());
+insert into contact_notes (content, contact_id, updated_at, created_at) values ('Sed ante. Vivamus tortor.', 8, now(), now());
+insert into contact_notes (content, contact_id, updated_at, created_at) values ('Aenean sit amet justo. Morbi ut odio.', 19, now(), now());
+insert into contact_notes (content, contact_id, updated_at, created_at) values ('In est risus, auctor sed, tristique in, tempus sit amet, sem. Fusce consequat.', 24, now(), now());
+insert into contact_notes (content, contact_id, updated_at, created_at) values ('Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices.', 23, now(), now());
+insert into contact_notes (content, contact_id, updated_at, created_at) values ('Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat. Curabitur gravida nisi at nibh.', 12, now(), now());
+insert into contact_notes (content, contact_id, updated_at, created_at) values ('Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus.', 16, now(), now());
+insert into contact_notes (content, contact_id, updated_at, created_at) values ('Integer a nibh. In quis justo. Maecenas rhoncus aliquam lacus.', 4, now(), now());
+insert into contact_notes (content, contact_id, updated_at, created_at) values ('Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet. Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo.', 11, now(), now());
+insert into contact_notes (content, contact_id, updated_at, created_at) values ('Proin at turpis a pede posuere nonummy. Integer non velit.', 14, now(), now());
+insert into contact_notes (content, contact_id, updated_at, created_at) values ('Duis at velit eu est congue elementum. In hac habitasse platea dictumst.', 1, now(), now());
+
+--
+-- Table: phone_numbers
+--
+
+insert into phone_numbers (country_code, region_code, number, contact_id) values (810, 7, '4709043625', 24);
+insert into phone_numbers (country_code, region_code, number, contact_id) values (1411, 46, '6794670190', 18);
+insert into phone_numbers (country_code, region_code, number, contact_id) values (1131, 98, '8317819267', 9);
+insert into phone_numbers (country_code, region_code, number, contact_id) values (1122, 30, '3815596487', 9);
+insert into phone_numbers (country_code, region_code, number, contact_id) values (1697, 11, '3255828852', 7);
+insert into phone_numbers (country_code, region_code, number, contact_id) values (1236, 86, '7414940298', 12);
+insert into phone_numbers (country_code, region_code, number, contact_id) values (1515, 79, '5473270517', 20);
+insert into phone_numbers (country_code, region_code, number, contact_id) values (453, 59, '2243368223', 16);
+insert into phone_numbers (country_code, region_code, number, contact_id) values (1235, 36, '4113359649', 25);
+insert into phone_numbers (country_code, region_code, number, contact_id) values (969, 82, '7595778237', 8);
+insert into phone_numbers (country_code, region_code, number, contact_id) values (1384, 53, '7448859776', 2);
+insert into phone_numbers (country_code, region_code, number, contact_id) values (1703, 34, '9719382513', 20);
+insert into phone_numbers (country_code, region_code, number, contact_id) values (1321, 97, '9301371805', 21);
+insert into phone_numbers (country_code, region_code, number, contact_id) values (1072, 39, '2151980869', 5);
+insert into phone_numbers (country_code, region_code, number, contact_id) values (106, 14, '2188748616', 20);
+insert into phone_numbers (country_code, region_code, number, contact_id) values (153, 59, '4508230835', 13);
+insert into phone_numbers (country_code, region_code, number, contact_id) values (1945, 1, '7869520394', 22);
+insert into phone_numbers (country_code, region_code, number, contact_id) values (1741, 72, '4151139831', 11);
+insert into phone_numbers (country_code, region_code, number, contact_id) values (835, 8, '4568445848', 19);
+insert into phone_numbers (country_code, region_code, number, contact_id) values (1569, 97, '6648989335', 11);
+insert into phone_numbers (country_code, region_code, number, contact_id) values (879, 33, '7484749397', 20);
+insert into phone_numbers (country_code, region_code, number, contact_id) values (936, 60, '3942502645', 13);
+insert into phone_numbers (country_code, region_code, number, contact_id) values (43, 6, '6244452163', 17);
+insert into phone_numbers (country_code, region_code, number, contact_id) values (711, 26, '5061221533', 3);
+insert into phone_numbers (country_code, region_code, number, contact_id) values (330, 91, '1932376369', 10);
+insert into phone_numbers (country_code, region_code, number, contact_id) values (241, 24, '9115758449', 3);
+insert into phone_numbers (country_code, region_code, number, contact_id) values (1931, 28, '9443421516', 2);
+insert into phone_numbers (country_code, region_code, number, contact_id) values (166, 9, '4977783917', 23);
+insert into phone_numbers (country_code, region_code, number, contact_id) values (368, 10, '6089923963', 3);
+insert into phone_numbers (country_code, region_code, number, contact_id) values (1, 14, '2232375737', 12);
+insert into phone_numbers (country_code, region_code, number, contact_id) values (691, 53, '6771033691', 4);
+insert into phone_numbers (country_code, region_code, number, contact_id) values (376, 85, '9928584596', 11);
+insert into phone_numbers (country_code, region_code, number, contact_id) values (1083, 17, '4476329371', 1);
+insert into phone_numbers (country_code, region_code, number, contact_id) values (535, 34, '2953344250', 14);
+insert into phone_numbers (country_code, region_code, number, contact_id) values (596, 91, '5268508261', 5);
+insert into phone_numbers (country_code, region_code, number, contact_id) values (285, 95, '4072722853', 25);
+insert into phone_numbers (country_code, region_code, number, contact_id) values (1288, 99, '1942371009', 16);
+insert into phone_numbers (country_code, region_code, number, contact_id) values (1008, 24, '2544887275', 3);
+insert into phone_numbers (country_code, region_code, number, contact_id) values (1093, 81, '2954136626', 20);
+insert into phone_numbers (country_code, region_code, number, contact_id) values (1161, 7, '6537985018', 15);
+
+--
+-- Table: contact_tag
+--
+
+insert into contact_tag (contact_id, tag_id) values (23, 7);
+insert into contact_tag (contact_id, tag_id) values (11, 6);
+insert into contact_tag (contact_id, tag_id) values (4, 8);
+insert into contact_tag (contact_id, tag_id) values (4, 6);
+insert into contact_tag (contact_id, tag_id) values (2, 3);
+insert into contact_tag (contact_id, tag_id) values (6, 3);
+insert into contact_tag (contact_id, tag_id) values (10, 10);
+insert into contact_tag (contact_id, tag_id) values (6, 5);
+insert into contact_tag (contact_id, tag_id) values (15, 5);
+insert into contact_tag (contact_id, tag_id) values (22, 5);
+insert into contact_tag (contact_id, tag_id) values (3, 7);
+insert into contact_tag (contact_id, tag_id) values (7, 5);
+insert into contact_tag (contact_id, tag_id) values (16, 3);
+insert into contact_tag (contact_id, tag_id) values (18, 5);
+insert into contact_tag (contact_id, tag_id) values (16, 5);
